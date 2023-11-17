a21ad039d0a71597db248b67b341725482f3dc43
weiraneve <907221539@qq.com>
2023-11-17 12:05:46 +0800

    feat: add env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, env};
+use actix_web::{App, HttpServer, get};
+
+#[get("/")]
+async fn index() -> String {
+    format!("Hello world")
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    dotenvy::dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("setting up app from environment");
+
+    let server_addr = env::var("SERVER_ADDR").expect("SERVER_ADDR is not set in .env file");
+
+    log::info!("starting HTTP server at {server_addr}");
+
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+    })
+        .bind(server_addr)?
+        .run()
+        .await
 }
