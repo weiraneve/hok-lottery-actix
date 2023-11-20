b6fc60b4196edbee8102270fb08965ada658e18e
weiraneve <907221539@qq.com>
2023-11-20 23:18:10 +0800

    refactor: integration reset service use

--- src/controller/reset_controller.rs
+++ src/controller/reset_controller.rs
@@ -3,7 +3,6 @@ use actix_web::{get, Responder, web};
 use crate::AppState;
 use crate::model::team_query::TeamQuery;
 use crate::service::reset_service;
-use crate::service::reset_service::reset_one_team;
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(reset_team);
@@ -16,7 +15,7 @@ pub async fn reset_team(
     query: web::Query<TeamQuery>,
     app_state: web::Data<AppState<'_>>,
 ) -> actix_web::Result<impl Responder> {
-    let response_data = reset_one_team(query.id, &app_state).await?;
+    let response_data = reset_service::reset_one_team(query.id, &app_state).await?;
     Ok(web::Json(response_data))
 }
 
